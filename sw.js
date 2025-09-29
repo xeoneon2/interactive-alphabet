25be89e59d754ef7afc73c155050cf1741b97d7b
xeoneon2 <dimans121@gmail.com>
2025-09-29 13:22:50 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('abc-cache').then(cache => {
       return cache.addAll([
-        '/',
+        './',
         './index.html',
         './manifest.json'
       ]);

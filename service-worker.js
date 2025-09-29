6065412074e85d249a04aecf137a28933491f713
xeoneon2 <dimans121@gmail.com>
2025-09-29 12:31:12 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,6 +10,7 @@ self.addEventListener('install', e => {
   );
 });
 
+
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(response => response || fetch(e.request))
